91dd3c94b4c01ac76e380b890779b2cfc69c4006
Lara Pontes <laradicp@gmail.com>
2022-05-20 16:54:40 -0300

    fix perturbation declaration

--- heuristic/src/Heuristic.h
+++ heuristic/src/Heuristic.h
@@ -25,7 +25,7 @@ void swapCL(std::vector<int> &s, Data &data, int beginSearch, int endSearch, std
 void swapCL(std::vector<int> &s, Data &data, int beginSearch1, int endSearch1, int beginSearch2, int endSearch2,
     std::list<std::pair<int, int>> &feasiblePairs, int &feasiblePairsSize);
 void swap(std::vector<int> &s, int p1, int p2);
-void perturbation(std::vector<int> &s, Data &data);
+void perturbation(std::vector<int> &s, Data &data, FeasiblePairsAnalysis &feasiblePairsAnalysis);
 int heuristic(Data data, std::vector<int> &bestS, FeasiblePairsAnalysis &feasiblePairsAnalysis);
 
 #endif
