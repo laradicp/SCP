f809017761f1262df14aa622e3c895ca7758c800
Lara Pontes <laradicp@gmail.com>
2022-05-19 23:37:04 -0300

    change vector from bool to char

--- heuristic/src/Data.h
+++ heuristic/src/Data.h
@@ -13,13 +13,13 @@ class Data
         int dimension;
         std::vector<std::pair<int, int>> cadences;
         int maxCadence;
-        std::vector<std::pair<std::vector<bool>, std::vector<int>>> families;
+        std::vector<std::pair<std::vector<char>, std::vector<int>>> families;
         int cadencesSize;
         int familiesSize;
         std::vector<int> familySize;
         std::vector<int> lb;
 
-        static bool compareCadencePairs(std::pair<std::pair<int, int>, bool> p1, std::pair<std::pair<int, int>, bool> p2);
+        static bool compareCadencePairs(std::pair<std::pair<int, int>, char> p1, std::pair<std::pair<int, int>, char> p2);
         static bool compareCadences(std::pair<int, int> c1, std::pair<int, int> c2);
         int used(int j, int i, std::vector<int> &s, std::vector<double> &score, std::vector<int> &jobsPerScore, std::vector<std::vector<int>> &familiesPerScore, std::vector<std::vector<int>> &intersection);
         void calculateLB(int i, std::vector<int> &s, std::vector<double> &score, std::vector<int> &jobsPerScore, std::vector<std::vector<int>> &familiesPerScore, std::vector<std::vector<int>> &intersection);
